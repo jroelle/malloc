52845566a7347c47caea7da39a587db32a01a6ff
jroelle <ngloukik@gmail.com>
2020-01-02 17:04:19 +0300

    realloc v0.0000001

--- src/realloc.c
+++ src/realloc.c
@@ -1,10 +1,19 @@
 #include "inc/malloc.h"
+#include <string.h>
+#define ft_memcpy memcpy
 
 void *realloc(void *ptr, size_t size)
 {
+	void *new_ptr;
+
 	lock_mutex();
-	if (!ptr)
-		return (malloc(size));
+	if (!size)
+		return (NULL);
+	new_ptr = malloc(size);
+	if (!ptr || !new_ptr)
+		return (new_ptr);
+	ft_memcpy(new_ptr, ptr, size);
+	free(ptr);
 	unlock_mutex();
 	return NULL;
 }
